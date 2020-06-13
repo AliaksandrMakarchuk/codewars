900ba77071e32df5fabdf12db2bbd59324e32091
Aliaksandr Makarchuk <Aliaksandr_Makarchuk@epam.com>
2020-06-14 00:19:51 +0300

    Finished Kata. Updated gitignore.

--- typescript/Temp/Temp.ts
+++
@@ -1,7 +0,0 @@
-export class Temporary {
-    public static pringMessage(message: string): void {
-        console.log(`Message: ${message}`);
-    }
-}
-
-console.log(Temporary.pringMessage('hello'));
